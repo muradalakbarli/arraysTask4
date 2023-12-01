6cb04e1b6bb608d4b42fcae5a8a962b00c9cc9e4
muradalakbarli <muradea@code.edu.az>
2023-12-01 20:40:04 +0400

    5inci tapsirig tamamlandi

---
+++ Task4-5/Program.cs
@@ -0,0 +1,17 @@
+﻿class Student
+{
+    static void Main()
+    {
+        string name = "Murad ";
+        string surname = "Alakbarli ";
+        string age = "15 ";
+        string speciality = "Programming ";
+        string groupNo = "P419";
+
+        Console.WriteLine( name + surname + age + speciality + groupNo );
+
+    }
+}
+
+   
+ 
